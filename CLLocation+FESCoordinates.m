25fa407d8e80704b3b034dd75aae5bfc18b884d0
Dan Weeks <dan@danimal.org>
2013-07-02 21:33:13 -0700

    break the license out to seperate file add Johnnie as a copyright holder too

--- CLLocation+FESCoordinates.m
+++ CLLocation+FESCoordinates.m
@@ -2,7 +2,7 @@
 //  CLLocation+FESCoordinates.m
 //
 //  Created by Dan Weeks on 2012-03-15.
-//  Copyright © 2012-2013 Daniel Weeks.
+//  Copyright © 2012-2013 Daniel Weeks & Johnnie Pittman.
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the “Software”), to deal
